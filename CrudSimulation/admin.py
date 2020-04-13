8967b41d83483ee2ba2eb07601de3c427532097b
Janrey Licas <janjan.licas@gmail.com>
2020-04-13 19:10:41 +0800

    Checkpoint | Django Administration is Finally Done

--- CrudSimulation/admin.py
+++ CrudSimulation/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+class UserDepartmentAttributes(admin.ModelAdmin):
+    model = UserDepartment
+    list_display = ("Department_Name", "Department_SName", "Department_UUID")
+    list_filter = ("Department_Name", "Department_SName", "Department_UUID")
+    readonly_fields = ("Department_UUID", )
+
+class UserRolesAttributes(admin.ModelAdmin):
+    model = UserRoles
+    list_display = ("Role_Name", "Role_Description")
+    list_filter = ("Role_Name", "Role_Description")
+
+class UserCredentialsAttributes(UserAdmin):
+    model = UserCredentials
+    list_display = ("first_name", "middle_name", "role", "dept_residence")
+    list_filter = ("first_name", "middle_name", "role", "dept_residence")
+    readonly_fields = ("uuid", )
+    fieldsets =  (
+        ('User Credentials', {
+            'fields': ('username', 'password', 'role', 'dept_residence'),
+            }
+        ),
+        ('User Information', {
+            'fields': ('first_name', 'middle_name', 'last_name', 'avatar'),
+            }
+        ),
+        ('Permissions and Restrictions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups'),
+            }
+        ),
+        ('Dates | Last Login and Creation Time', {
+            'fields': ('last_login', 'date_joined'),
+            }
+        ),
+        ('Technical Information', {
+            'fields': ('uuid',),
+            }
+        ),
+    )
+    add_fieldsets = (
+        ('User Credentials', {
+            'fields': ('username', 'password1', 'password2', 'role', 'dept_residence'),
+            }
+        ),
+        ('User Information', {
+            'fields': ('first_name', 'middle_name', 'last_name', 'avatar'),
+            }
+        ),
+        ('Permissions and Restrictions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups'),
+            }
+        ),
+        ('Technical Information', {
+            'fields': ('uuid',),
+            }
+        ),
+    )
+
+class UserTasksAttributes(admin.ModelAdmin):
+    model = UserTasks
+    list_display = ("Task_Name", "Task_Description", "Task_Type")
+    list_filter = ("Task_Name", "Task_Description", "Task_Type")
+admin.site.register(UserDepartment, UserDepartmentAttributes)
+admin.site.register(UserRoles, UserRolesAttributes)
+admin.site.register(UserCredentials, UserCredentialsAttributes)
+admin.site.register(UserTasks, UserTasksAttributes)
--- CrudSimulation/models.py
+++ CrudSimulation/models.py
@@ -13,7 +13,7 @@ class UserDepartment(models.Model):
         db_table = "user_department"
 
     Department_Name = models.CharField(max_length=32, null=False, blank=False, verbose_name="Department Name", help_text="Department Unique Identity. Make it formalized by using Title Case as possible.")
-    Department_SName = models.CharField(max_length=16, null=False, blank=False, verbose_name="Department CodeName", help_text="Department Description. This describes the department's objective and goal. Actually you can write anything that you like.")
+    Department_SName = models.CharField(max_length=16, null=False, blank=False, unique=True, verbose_name="Department CodeName", help_text="Department Description. This describes the department's objective and goal. Actually you can write anything that you like.")
     Department_UUID = models.UUIDField(max_length=32, default=uuid4, editable=False, unique=True, help_text="A Unique Identifier for the Department. Used for references and any other such.")
 
     def __str__(self):
@@ -21,10 +21,10 @@ class UserDepartment(models.Model):
 
 class UserRoles(models.Model):
     class Meta:
-        verbose_name = "User Roles"
+        verbose_name = "User Role"
         db_table = "user_roles"
 
-    Role_Name = models.CharField(max_length=32, null=False, blank=False, verbose_name="User Role Name", help_text="A group-scoped identifier used to name what they really are. Selected Role is allowed.")
+    Role_Name = models.CharField(max_length=32, null=False, blank=False, unique=True, verbose_name="User Role Name", help_text="A group-scoped identifier used to name what they really are. Selected Role is allowed.")
     Role_Description = models.CharField(max_length=64, null=False, blank=False, verbose_name="User Role Description", help_text="A identifier description. Write anything that you like here based from what you interpret to this particular role.")
 
     def __str__(self):
@@ -42,8 +42,8 @@ class UserCredentials(AbstractUser):
 
     middle_name = models.CharField(max_length=20, null=True, blank=True, help_text="A Name that is not supplied by User Model. Added for Unique User Purposes.")
     uuid = models.UUIDField(max_length=32, default=uuid4, editable=False, unique=True, help_text="A Unique Identifier for your Account. This is used for DB authentication and references. Please DO NOT SHARE IT OUTSIDE.")
-    role = models.ForeignKey(UserRoles, to_field="Role_Name", null=False, blank=False, verbose_name="User Role", help_text="A Group-Scoped Identifier for who should be dominating at your group.", on_delete=models.CASCADE)
-    dept_residence = models.ForeignKey(UserDepartment, to_field="Department_SName", null=False, blank=False, verbose_name="User Assigned Deparment", help_text="Assigned Deparment. Literally a department assigned to this particular person.", on_delete=models.CASCADE)
+    role = models.ForeignKey(UserRoles, to_field="Role_Name", null=True, blank=True, verbose_name="User Role", help_text="A Group-Scoped Identifier for who should be dominating at your group.", on_delete=models.CASCADE)
+    dept_residence = models.ForeignKey(UserDepartment, to_field="Department_SName", null=True, blank=True, verbose_name="User Assigned Deparment", help_text="Assigned Deparment. Literally a department assigned to this particular person.", on_delete=models.CASCADE)
     avatar = models.ImageField(null=True, blank=True, upload_to=user_src_avatar_path)
 
     def __str__(self):
@@ -51,12 +51,15 @@ class UserCredentials(AbstractUser):
 
 class UserTasks(models.Model):
     class Meta:
-        verbose_name = "User Tasks"
+        verbose_name = "User Task"
         db_table = "user_tasks"
 
-    Task_Name = models.CharField(max_length=32, null=False, blank=False, verbose_name="", help_text="")
-    Task_Description = models.CharField(max_length=256, null=False, blank=False, verbose_name="", help_text="")
-    Task_Type = models.CharField(max_length=16, null=False, blank=False, verbose_name="", help_text="")
+    Task_Name = models.CharField(max_length=32, null=False, blank=False, verbose_name="Task Name", help_text="Unique Identifier for Task Assigned To Users.")
+    Task_Description = models.CharField(max_length=256, null=False, blank=False, verbose_name="Task Description", help_text="Literally Task Description. Required. You have to point out what's the point of the task given to you.")
+    Task_Type = models.CharField(max_length=16, null=False, blank=False, verbose_name="Task Type", choices=TaskTypes, default=TaskTypes[0][0], help_text="Literally A Task Type. This could help users how important the task is.")
+    Task_StartTime = models.DateTimeField(verbose_name='Task Start Time', help_text='Refers to a time start of the task.', null=True, blank=True)
+    Task_EndTime = models.DateTimeField(verbose_name='Task End Time', help_text='Refers to a time end of the task.', null=True, blank=True)
+    Task_CreateDate = models.DateTimeField(auto_now_add=True, verbose_name='Task Creation Date & Time', help_text="Refers to a time from where the task has been created.", null=False, blank=False, editable=False)
 
     def __str__(self):
-        return '%s | %s | %s' % (self.Task_Name, self.Task_Description, self.Task_Type)
+        return '%s | %s | %s |> Created as of ' % (self.Task_Name, self.Task_Description, self.Task_Type, self.Task_CreateDate)
