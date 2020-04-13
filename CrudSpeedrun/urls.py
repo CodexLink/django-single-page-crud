b675e8a7a71defa8b336a6a40237bb736813f240
Janrey Licas <janjan.licas@gmail.com>
2020-04-13 22:29:57 +0800

    URL at Root Level | Added Userdata Accesibility on Web

--- CrudSpeedrun/urls.py
+++ CrudSpeedrun/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', include('CrudSimulation.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
