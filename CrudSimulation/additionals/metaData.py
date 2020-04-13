4c8626926758dce828ac53791502b115a060f714
Janrey Licas <janjan.licas@gmail.com>
2020-04-13 13:20:11 +0800

    Additionals | Init

---
+++ CrudSimulation/additionals/metaData.py
@@ -0,0 +1,15 @@
+UserData = (
+    ("Leader", "Leader"),
+    ("Co-Leader", "Co-Leader"),
+    ("Project Assistant", "Project Assistant"),
+    ("Project Core", "Project Core"),
+    ("Treasurer", "Treasurer"),
+    ("Team Support", "Team Support"),
+    ("Participant", "Participant"),
+    ("Substitute", "Substitute"),
+    ("Task Manager", "Task Manager"),
+    ("Project Manager", "Project Manager"),
+    ("Documentator", "Documentator"),
+    ("General Researcher", "General Researcher"),
+    ("Resource Researcher", "Resource Researcher"),
+)
